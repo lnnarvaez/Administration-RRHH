ea6b6a7c79899fb09a8197e1871d239bee656026
lnnarvaez <lnnarvaez@unan.edu.ni>
2026-03-27 12:31:50 -0600

    Se ha complementado la estructura de las clases: Departament, Recruitment y JobTitle. Se Diseña la vista para la captura de datos.

--- Domain/JobTitle.cs
+++ Domain/JobTitle.cs
@@ -7,5 +7,38 @@ namespace Administration_RRHH.Domain
 {
     public class JobTitle
     {
+        public JobTitle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int JobCode
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int JobLevel
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int BaseSalary
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void AddJobTitle()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- UI/Catalogs/FrmEmployee.Designer.cs
+++ UI/Catalogs/FrmEmployee.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SuspendLayout();
+            // 
+            // FrmEmployee
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(707, 410);
+            Name = "FrmEmployee";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
