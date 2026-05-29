6349c01f147f2c71775357f9d62d24c50c889e39
Lawdee Norman Narváez Bello <lnnarvaez@unan.edu.ni>
2026-05-29 12:25:52 -0600

    Rectificar la conexión con la base de datos, Definir propiedades en la clase Region y establecer directorio de logica de negocio

---
+++ Business/RegionBusiness.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration_RRHH.Application
+{
+    internal class RegionBusiness
+    {
+        /* --------------------------------------------------------*/
+        /*      ----    Declaración de propiedades      ----      */
+        /* --------------------------------------------------------*/
+
+        public int RegionId { get; set; }
+        public string RegionCode { get; set; } 
+        public string Description { get; set; }
+        public bool Disponibilidad { get; set; }
+
+        /* --------------------------------------------------------*/
+        /*      ----    Constructores      ----                    */
+        /* --------------------------------------------------------*/
+
+        // Constructor sin parámetros
+        public RegionBusiness()
+        {
+            // Constructor por defecto
+        }
+
+        public RegionBusiness(int regionId, string regionCode, string description, bool disponibilidad)
+        {
+            RegionId = regionId;
+            RegionCode = regionCode;
+            Description = description;
+            Disponibilidad = disponibilidad;
+        }
+
+        /* --------------------------------------------------------*/
+        /*      ----    Métodos             ---                    */
+        /* --------------------------------------------------------*/
+
+    }//End class
+}//end namespace
