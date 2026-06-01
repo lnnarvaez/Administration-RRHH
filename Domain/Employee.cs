b944829a1092e65716da9211b7426c4c3e510687
Lawdee Norman Narváez Bello <lnnarvaez@unan.edu.ni>
2026-06-01 16:13:52 -0600

    Redefinir la clase Employee a partir de la herencia de Individual

--- Domain/Employee.cs
+++ Domain/Employee.cs
@@ -6,9 +6,96 @@ using System.Text.Json;
 
 namespace Administration_RRHH.Domain
 {
-    public class Employee
+    public class Employee : Individual
     {
-       
+
+        /* -------------------------------------------------------------------------- */
+        /*                      Declaración de Propiedades                            */
+        /* -------------------------------------------------------------------------- */
+
+        public string Inss { get; set; }
+        public string Email { get; set; }
+        public int NumberChildren { get; set; }
+        public bool IsActive { get; set; }
+        
+
+        /* -------------------------------------------------------------------------- */
+        /*                      Declaración de Constructor                           */
+        /* -------------------------------------------------------------------------- */
+        public Employee() { }
+
+        public Employee(string inss, string email, bool isActive, int numberChildren,
+                        string idNumber, string name, string surname, DateOnly birthdate,
+                        string phone, string address) :
+                        base(idNumber, name, surname, birthdate, phone, address)
+        {
+            Inss = inss;
+            Email = email;
+            IsActive = isActive;
+            NumberChildren = numberChildren;
+        }
+
+        /* -------------------------------------------------------------------------- */
+        /*                           Declaración de Método                            */
+        /* -------------------------------------------------------------------------- */
+
+        /// <summary>
+        /// Valida que la fecha de nacimiento del empleado no sea una fecha futura.
+        /// </summary>
+        /// <returns>Verdadero si la fecha es válida, falso en caso contrario</returns>
+        public override bool ValidateBirthDate()
+        {
+            // Validar que la fecha de nacimiento no sea una fecha futura
+            if (BirthDate > DateOnly.FromDateTime(DateTime.Today))
+            {
+                return false; // La fecha de nacimiento es inválida
+            }
+            return true; // La fecha de nacimiento es válida
+        }
+
+        public int AddEmployee  ()
+        {
+            return 0; //Pendiente de implementación con la base de datos
+        }
+
+        /// <summary>
+        /// Lee el registro de un empleado especificado por su número de cédula
+        /// </summary>
+        /// <param name="idNumber">Número de cédula para filtrar al empleado</param>
+        /// <returns>Empleado identificado por cédula. Null sino se encuentra</returns>
+        public Employee GetEmployeeIdNumber (int idNumber)
+        {
+            return new Employee(); //Temporal mientras se defien BD
+        }
+
+        /// <summary>
+        /// Lista un set de Empleados, segmentados por bloques de páginación 10 en 10
+        /// </summary>
+        /// <returns>Listado de Empleados ordenados de forma descendente por fecha de ingreso</returns>
+        public List <Employee> ListEmployee ()
+        { 
+            return new List<Employee>(); // Pendiente de implementación con la base de datos
+        }
+
+        /// <summary>
+        /// Modifica el registro de un empleado especificado por su número de cédula. 
+        /// El método recibe el número de cédula del empleado a modificar y los nuevos 
+        /// datos del empleado.
+        /// </summary>
+        /// <param name="idNumber"></param>
+        public void ModifyEmployee (string idNumber)
+        {
+            // Pendiente de implementación con la base de datos
+        }
+
+        /// <summary>
+        /// Deshabilita el registro de un empleado especificado por su número de cédula.
+        /// </summary>
+        /// <param name="idNumber"></param>
+        public void DisableEmployee(string idNumber)
+        {
+            // Pendiente de implementación con la base de datos
+        }   
 
 
     }//end-class
---
+++ Services/BusinessLogic/EmployeeBusiness.cs
@@ -0,0 +1,50 @@
+﻿using Administration_RRHH.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration_RRHH.Services.BusinessLogic
+{
+    internal class EmployeeBusiness
+    {
+        Employee employee; //Instancia de la clase Employee para realizar
+                           //operaciones relacionadas con los empleados
+
+        public EmployeeBusiness ()
+        {
+            employee = new Employee ();
+        }
+
+        public int InsertEmployee()
+        {
+           if (employee.AddEmployee() > 0)
+            return 1; //Pendiente de implementación con la base de datos
+           else
+            return 0;   
+        }
+
+
+        public Employee ReadEmployee(string idNumber)
+        {
+            return new Employee(); //Pendiente de implementación con la base de datos
+        }       
+
+        public List<Employee> ListEmployees(int pageNumber)
+        {
+            return new List<Employee>(); //Pendiente de implementación con la base de datos
+        }
+
+        public int UpdateEmployee(string idNumber)
+        {
+            return 0;
+        }
+
+        public int DeleteEmployee(string idNumber)
+        {
+            return 0;
+        } 
+
+    } //end-class
+} //end-namespace
